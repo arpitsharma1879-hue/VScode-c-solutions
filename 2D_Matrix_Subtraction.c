33cf8315d9a3d20b72d73142285139b5b5b54aa8
arpitsharma1879-hue <arpitsharma1879@gmail.com>
2026-08-23 20:32:24 +0530

    Add files via upload

---
+++ 2D_Matrix_Subtraction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a[10][10], b[10][10], r, c, i, j;
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+    printf("Enter first matrix:\n");
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
+            scanf("%d", &a[i][j]);
+    printf("Enter second matrix:\n");
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
+            scanf("%d", &b[i][j]);
+    printf("Subtraction matrix:\n");
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+            printf("%d ", a[i][j] - b[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
